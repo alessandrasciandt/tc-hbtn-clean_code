30656e45c93169cee8777d4dd553169051f1d220
alessandrasciandt <alessandras@ciandt.com>
2022-07-18 12:07:43 -0400

    Comite 1. Funções

---
+++ 0x00/function/Functions.java
@@ -0,0 +1,41 @@
+public class Functions {
+    // Faça somente 1 coisa
+    //B
+    public void saveIncome(Income income){
+        inRepo.save(income);
+    }
+    public void saveExpense(Expense expense){
+        outRepo.save(expense);
+    }
+
+    //Somente 1 nível de abstração
+    //B
+    @PostMapping("/user")
+    public User saveUser(User user){
+        return userService.userRepository.save(user);
+    }
+
+    // Arguments
+    //A
+    public void saveUser(User user){};
+
+    //  No side effects
+    //B
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+    public void setRole(User user){
+        user.setRole(user);
+    }
+
+    // Evite duplicados, extraindo para novos métodos
+    //A
+    public Environment getEnvironment(){
+        String version = versionService.getVersion();
+        return environmentService.getEnvironment(version);
+    }
+    public Release getRelease(){
+        String version = versionService.getVersion();
+        return releaseService.getRelease(version);
+    }
+}
